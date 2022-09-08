fe187d631cddc8e2a86bfc4be9f15d9b40892901
Modovaci-Strom <113085334+Modovaci-Strom@users.noreply.github.com>
2022-09-08 13:33:55 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "A Prestige Tree",
 	id: "mymod",
-	author: "nobody",
+	author: "Author McAuthorface",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
